8c2888c67bf3d0a44f9f00bc4d378e7044edd3a2
Henry <hi@henry.wang>
2018-09-04 22:38:06 +0100

    Update nav

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,7 +3,7 @@
     <div class="navbar-brand">
       <a class="navbar-item" href="/">
         <span class="tag is-rounded is-white is-medium">
-        <img src="/logo.jpg" width="28" height="28">Travel</span>
+        <img src="/logo.jpg" width="28" height="28">✈️Travel</span>
       </a>
       <div class="navbar-burger burger" data-target="navbarData">
         <span/>
@@ -14,9 +14,19 @@
 
     <div id="navbarData" class="navbar-menu">
       <div class="navbar-start">
-        <a class="navbar-item" href="/">
-          Home
-        </a>
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link" href="/documentation/overview/start/">
+            More
+          </a>
+          <div class="navbar-dropdown is-boxed">
+            <a class="navbar-item" href="https://henry.wang">
+              Blog
+            </a>
+            <a class="navbar-item" href="ttps://api.henry.wang">
+              API
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </nav>
