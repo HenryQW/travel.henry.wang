f7709ba2eb8c52cfe09d120e2efe751c8c319788
Henry <hi@henry.wang>
2018-09-04 17:54:29 +0100

    Add icon interactivities

--- components/Map.vue
+++ components/Map.vue
@@ -20,6 +20,43 @@ export default {
         zoom: 2
       })
       map.addControl(new mapboxgl.NavigationControl())
+
+      map.on("mouseover", "symbols", function(e) {
+        console.log(e)
+        map.getCanvas().style.cursor = "pointer"
+      })
+
+      // Change it back to a pointer when it leaves.
+      map.on("mouseleave", "symbols", function(e) {
+        console.log(e)
+        map.getCanvas().style.cursor = ""
+      })
+
+      map.on("click", function(e) {
+        var features = map.queryRenderedFeatures(e.point, {
+          layers: ["travels"]
+        })
+        if (!features.length) {
+          return
+        }
+
+        var feature = features[0]
+        map.flyTo({
+          center: feature.geometry.coordinates,
+          zoom: 4,
+          bearing: 0,
+          speed: 1,
+          curve: 2,
+          easing: function(t) {
+            return t
+          }
+        })
+        new mapboxgl.Popup({ offset: [0, -15] })
+          .setLngLat(feature.geometry.coordinates)
+          .setHTML("<h3>" + feature.properties.place_name + "</h3>")
+          .setLngLat(feature.geometry.coordinates)
+          .addTo(map)
+      })
     }
   }
 }
